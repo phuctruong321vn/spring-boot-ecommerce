4a54b5e71eb37bd4f451b4701e113a5742bdd414
Phúc Đỗ <138603231+phuctruong321vn@users.noreply.github.com>
2025-07-14 01:51:10 +0700

    Add files via upload

---
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -0,0 +1,48 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message = "Street name must be atleast 5 characters")
+    private String street;
+
+
+    @NotBlank
+    @Size(min = 4, message = "City name must be atleast 4 characters")
+    private String city;
+
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be atleast 2 characters")
+    private String country;
+
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(String street, String buildingName, String city, String state, String country, String pincode) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+}
